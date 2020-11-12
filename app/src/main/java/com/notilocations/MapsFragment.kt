cd17c33245da707dd27e5c4d3b6255f414789165
Slad3 <feba23@live.com>
2020-11-11 18:37:38 -0600

    Added options to maps fragment

--- app/src/main/java/com/notilocations/MapsFragment.kt
+++ app/src/main/java/com/notilocations/MapsFragment.kt
@@ -1,5 +1,7 @@
 package com.notilocations
 
+import android.Manifest
+import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -14,7 +16,9 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap;
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -26,6 +30,23 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+
+        map = googleMap
+
+//        enableMyLocation()
+        // Set all the settings of the map to match the current state of the checkboxes
+        with(map.uiSettings) {
+            isZoomControlsEnabled = true
+            isCompassEnabled = true
+            isMyLocationButtonEnabled = true
+            isIndoorLevelPickerEnabled = true
+            isMapToolbarEnabled = true
+            isZoomGesturesEnabled = true
+            isScrollGesturesEnabled = true
+            isTiltGesturesEnabled = true
+            isRotateGesturesEnabled = true
+        }
+
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
@@ -36,6 +57,7 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
@@ -43,5 +65,47 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
     }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+
+
+
+    }
+
+
+
+//    /** Override the onRequestPermissionResult to use EasyPermissions */
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+//    }
+
+    /**
+     * enableMyLocation() will enable the location of the map if the user has given permission
+     * for the app to access their device location.
+     * Android Studio requires an explicit check before setting map.isMyLocationEnabled to true
+     * but we are using EasyPermissions to handle it so we can suppress the "MissingPermission"
+     * check.
+     */
+//    @SuppressLint("MissingPermission")
+//    @AfterPermissionGranted(REQUEST_CODE_LOCATION)
+//    private fun enableMyLocation() {
+//        if (hasLocationPermission()) {
+//            map.isMyLocationEnabled = true
+//        } else {
+//            EasyPermissions.requestPermissions(this, getString(R.string.location),
+//                REQUEST_CODE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
+//            )
+//        }
+//    }
+//
+//    private fun hasLocationPermission(): Boolean {
+//        return EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION)
+//    }
 }
