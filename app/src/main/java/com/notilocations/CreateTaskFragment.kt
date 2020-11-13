70aabd4073ae126d20eb0a5ed8e2024fb8e0d9a8
WagnerBryce <digitalambassador0@gmail.com>
2020-11-13 01:43:49 -0600

    Revert "Implemented navigation to the create task fragment and back to the viewPager"
    This reverts commit 45da6636

--- app/src/main/java/com/notilocations/CreateTaskFragment.kt
+++ app/src/main/java/com/notilocations/CreateTaskFragment.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import com.notilocations.databinding.FragmentCreateTaskBinding
 
 class CreateTaskFragment : Fragment() {
 
@@ -16,10 +13,6 @@ class CreateTaskFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentCreateTaskBinding>(inflater, R.layout.fragment_create_task, container, false)
-        binding.submitTask.setOnClickListener{v: View ->
-            v.findNavController().navigate(R.id.action_createTaskFragment_to_swipeView)
-        }
-        return binding.root
+        return inflater.inflate(R.layout.fragment_create_task, container, false)
     }
 }
