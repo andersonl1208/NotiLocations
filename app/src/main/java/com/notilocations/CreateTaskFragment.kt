ae55e184e0984b04fbf060d14d6d0b4fb90089aa
WagnerBryce <digitalambassador0@gmail.com>
2020-11-13 02:10:17 -0600

    Lots of late night git struggles This should be exactly how it was.  I was having issues with the gradle sync and are unsure if you guys will beacuse the way it started happening was super odd. This is the OG gradle files tho

--- app/src/main/java/com/notilocations/CreateTaskFragment.kt
+++ app/src/main/java/com/notilocations/CreateTaskFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.notilocations.databinding.FragmentCreateTaskBinding
 
 class CreateTaskFragment : Fragment() {
 
@@ -13,6 +16,10 @@ class CreateTaskFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_create_task, container, false)
+        val binding = DataBindingUtil.inflate<FragmentCreateTaskBinding>(inflater, R.layout.fragment_create_task, container, false)
+        binding.submitTask.setOnClickListener{v: View ->
+            v.findNavController().navigate(R.id.action_createTaskFragment_to_swipeView)
+        }
+        return binding.root
     }
 }
--- app/src/main/java/com/notilocations/TaskListFragment.kt
+++ app/src/main/java/com/notilocations/TaskListFragment.kt
@@ -1,10 +1,17 @@
 package com.notilocations
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.notilocations.databinding.FragmentCreateTaskBinding
+import com.notilocations.databinding.FragmentTaskListBinding
+import kotlinx.android.synthetic.main.fragment_task_list.view.*
 
 class TaskListFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +19,10 @@ class TaskListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_task_list, container, false)
+        val binding = DataBindingUtil.inflate<FragmentTaskListBinding>(inflater, R.layout.fragment_task_list, container, false)
+        binding.addTask.setOnClickListener{v: View ->
+            v.findNavController().navigate(R.id.action_swipeView_to_createTaskFragment)
+        }
+        return binding.root
     }
 }
