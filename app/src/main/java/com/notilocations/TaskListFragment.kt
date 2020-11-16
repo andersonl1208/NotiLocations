adc2d45323c24f4dc977048bff89ed18cb61e9a6
andersonl1208 <andersonl1208a@gmail.com>
2020-11-15 22:09:18 -0600

    Initial settings menu

--- app/src/main/java/com/notilocations/TaskListFragment.kt
+++ app/src/main/java/com/notilocations/TaskListFragment.kt
@@ -1,17 +1,13 @@
 package com.notilocations
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import com.notilocations.databinding.FragmentCreateTaskBinding
 import com.notilocations.databinding.FragmentTaskListBinding
-import kotlinx.android.synthetic.main.fragment_task_list.view.*
 
 class TaskListFragment : Fragment() {
     override fun onCreateView(
