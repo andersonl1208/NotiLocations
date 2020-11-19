7bbf9ab1c6789891a9689838e0fd8a90df0c397a
Ben <feba23@live.com>
2020-11-18 18:19:18 -0600

    Updating Android Manifest and Maps fragment

--- app/src/main/java/com/notilocations/MapsFragment.kt
+++ app/src/main/java/com/notilocations/MapsFragment.kt
@@ -8,13 +8,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.*
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
 
@@ -26,7 +25,7 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         map.setOnMapLongClickListener { latLng: LatLng? ->
             println("long click")
             if (latLng != null) {
-                println("NotiLocation: " + latLng.latitude + latLng.longitude)
+                println("NotiLocation: " + latLng.latitude + "\t" + latLng.longitude)
 
                 // TODO Luke find a way to put this in the database
 
@@ -34,15 +33,16 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
                 val tempMarker = latLng
                 googleMap.addMarker(MarkerOptions().position(tempMarker).title("Luke's House"))
                 googleMap.moveCamera(CameraUpdateFactory.newLatLng(tempMarker))
-
-
             }
          }
 
 
-//        enableMyLocation()
+        enableMyLocation()
 
 
+        val tempLocation = LatLng(44.87335854645772, -91.9216525554657)
+        googleMap.addMarker(MarkerOptions().position(tempLocation).title("Luke's House"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(tempLocation))
         // Set all the settings of the map to match the current state of the checkboxes
         with(map.uiSettings) {
             isZoomControlsEnabled = true
@@ -59,7 +59,7 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         // Example Marker
 //        val sydney = LatLng(-34.0, 151.0)
 //        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
     }
 
     override fun onCreateView(
@@ -103,19 +103,19 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
      * but we are using EasyPermissions to handle it so we can suppress the "MissingPermission"
      * check.
      */
-//    @SuppressLint("MissingPermission")
-//    @AfterPermissionGranted(REQUEST_CODE_LOCATION)
-//    private fun enableMyLocation() {
-//        if (hasLocationPermission()) {
-//            map.isMyLocationEnabled = true
-//        } else {
-//            EasyPermissions.requestPermissions(this, getString(R.string.location),
-//                REQUEST_CODE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
-//            )
-//        }
-//    }
-//
-//    private fun hasLocationPermission(): Boolean {
-//        return EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION)
-//    }
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(R.string.location)
+    private fun enableMyLocation() {
+        if (hasLocationPermission()) {
+            map.isMyLocationEnabled = true
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.location), 0,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return EasyPermissions.hasPermissions(this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+    }
 }
